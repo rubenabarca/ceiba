7231a910f78ff052ca6f7fd3b9ae2ddd620bc3a2
Rubén Abarca <rudavid0400@gmail.com>
2021-05-03 18:28:19 -0600

    obtainMultiplesOf3Or5AndTheSum

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Euler0001',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+          title:
+              'Exercise: Euler0001-Find the sum of all multiples of three or five below one thousand'),
     );
   }
 }
@@ -27,12 +29,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Iterable<int> obtainMultiplesOf3Or5AndTheSum(int limit) sync* {
+    int index = 3;
+    int sum = 0;
+    do {
+      if (index % 3 == 0 || index % 5 == 0) {
+        sum += index;
+        yield index;
+      }
+      index++;
+    } while (index < 1000);
+    yield sum;
   }
 
   @override
@@ -44,19 +51,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          children: <Widget>[],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {},
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
